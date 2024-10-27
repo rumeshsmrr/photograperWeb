d7d65a2b5f47e3832f19e95da0987ef995e525f7
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2024-10-27 20:46:25 +0530

    add animatins to caegories component, create services file

---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Services() {
+  return (
+    <div className='pt-2 pl-10 pr-10 pb-10 mt-10 absolute bg-inherit w-full h-fit flex flex-col justify-center'>
+        <div className='font-regular text-[60px]  titleLineSpace'>
+        What we <span className="text-accent">Offer
+        </span>
+        </div>
+    </div>
+  )
+}
